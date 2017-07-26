bd35c11b20e01b0ca568601a0127a5085720da46
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-07-26 06:01:21 +0300

    Define controller by uri in run method

--- Application/App.php
+++ Application/App.php
@@ -7,6 +7,17 @@ class App
     // Запустить приложение
     public function run()
     {
+        $url_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         
+        $api_spec = '~^/(?<controller>[a-zA-Z0-9_]+)(?:/(?<method>[a-zA-Z0-9_]+))?/?.*$~';
+        preg_match($api_spec, $url_path, $matches);
+        
+        $controller_name = 'Application\\Controllers\\'.$matches['controller'].'Controller';
+        
+        if (class_exists($controller_name)){
+            echo 'Application\\Controllers\\'.$controller_name;
+        }
+        
+        $controller = new $controller_name();
     }
 }
