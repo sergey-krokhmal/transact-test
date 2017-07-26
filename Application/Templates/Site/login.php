2e43a90088f3d8254e573f88cffc37f7ad390c0b
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-07-26 13:16:37 +0300

    Add login form template

---
+++ Application/Templates/Site/login.php
@@ -0,0 +1,8 @@
+<h2>Вход в аккаунт</h2>
+<form method="POST" action="/Site/login">
+	<label>Логин</label>
+	<input name="username"></input>
+	<label>Пароль</label>
+	<input name="password" type="password"></input>
+	<button name="login">Войти</button>
+</form>
